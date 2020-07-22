e0ccfa3dee5d671cc1395493ea268afb2e9cf79a
Zachary Vernec <zachary.vernec@gmail.com>
2020-07-22 15:04:52 -0400

    Added TODO

--- smile/global_params.py
+++ smile/global_params.py
@@ -1,4 +1,4 @@
-# Simulations study parameters
+# Study parameters
 
 FMIN = 6 #minimum possible visual score
 SMIN = 0 #minimum possible symptom score
@@ -6,7 +6,9 @@ NDAYS = 160 #number of days in the study
 FIRSTVISIT = 8
 LASTVISIT = NDAYS-1
 
-# Simulatin graphing parameters
+# Graphing parameters
 
 lines_cmap_name = 'Dark2' #https://matplotlib.org/2.0.1/users/colormaps.html#qualitative
 points_cmap_name = 'viridis' #https://matplotlib.org/2.0.1/users/colormaps.html#sequential
+
+#TODO add title colors, hpadding and vpadding 
