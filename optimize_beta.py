2a63a88293cec2bb2e56bb4635c77039f721dd21
Zachary Vernec <zachary.vernec@gmail.com>
2022-06-14 18:33:59 -0400

    Argparse and refactoring

--- optimize_beta.py
+++ optimize_beta.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import argparse
 
 np.random.seed(20220609)
-rng = np.random.default_rng(67890)
+rng = np.random.default_rng(67890) #TODO USE
 
 # To access number of function evals in optim_fun
 class Counter:
@@ -16,24 +16,22 @@ class Counter:
         self.val += 1
 
 
+
+
 # Methods to get probabilities of early & late recovery
-def get_tails_empir(alpha, beta, eta, gamma, size=10000):
+def get_tails_empir(R, V_0, size=10000):
     '''
         Calculate tails by empirical ratio
 
         Parameters:
-            alpha, beta (float > 0, float > 0): parameters of rate rv R
-            eta, gamma (float > 0, float > 0): parameters of initial value rv V_0
+            R: frozen distribution for rate random variable
+            V_0: frozen distribution for initial value random variable
             size: amount of points in simulation
 
         Returns:
             early_prob: probability of -R*t+V_0 hitting 6 before day 7
             late_prob: probability of -R*t+V_0 hitting 6 after day 159
     '''
-    # Random variables for visual score defined in simulating_and_pickling as -R*t+V_0
-    # Have support [loc, loc+scale]
-    R = stats.beta(alpha, beta, loc=0, scale=2)
-    V_0 = stats.beta(eta, gamma, loc=14, scale=11)
     #Random variates
     r = R.rvs(size=size)
     v_0 = V_0.rvs(size=size)
@@ -43,22 +41,19 @@ def get_tails_empir(alpha, beta, eta, gamma, size=10000):
     late_prob = np.mean(-r*159 + v_0 > 6) #TODO use smile.global_params
 
     return early_prob, late_prob
-def get_tails_integ(alpha, beta, eta, gamma):
+def get_tails_integ(R, V_0):
     '''
         Calculate tails by integrating joint pdf
 
         Parameters:
-            alpha, beta (float > 0, float > 0): parameters of rate rv R
-            eta, gamma (float > 0, float > 0): parameters of initial value rv V_0
+            R: frozen distribution for rate random variable
+            V_0: frozen distribution for initial value random variable
+            size: amount of points in simulation
 
         Returns:
             early_prob: probability of -R*t+V_0 hitting 6 before day 7
             late_prob: probability of -R*t+V_0 hitting 6 after day 159
     '''
-    # Random variables for visual score defined in simulating_and_pickling as -R*t+V_0
-    # Have support [loc, loc+scale]
-    R = stats.beta(alpha, beta, loc=0, scale=2)
-    V_0 = stats.beta(eta, gamma, loc=14, scale=11)
 
     # Integrate to get probabilities based on distributions
     # The true formula is a double integral, but the first integral simply calculates a cdf or sf=1-cdf
@@ -67,70 +62,103 @@ def get_tails_integ(alpha, beta, eta, gamma):
 
     return early_prob, late_prob
 
-
-# Get which get_tails function to use
-parser = argparse.ArgumentParser(description='Optimize beta distributions of visual score parameters')
-parser.add_argument('--empirical', type=int, help='instead of integration, simulate with n points')
-empirical = parser.parse_args().empirical
+def error_fun(probs, targets):
+    ''' Square Error'''
+    return np.sum((np.array(probs) - np.array(targets))**2)
 
 
-# Error function to optimize 
-def optim_fun(params, early_target=0.3, late_target=0.1, counter=Counter(), print_frequency=0):
-    '''
-    Square Error func for fitting previous function
+if __name__ == '__main__':
 
-    Keep in mind this problem is underspecified, as len(params)==4 but there are only 2 target probabilities
+    # Optimization parameters
+    parser = argparse.ArgumentParser(description='Optimize beta distributions R and V_0')
+    parser.add_argument('method', choices=['integration', 'simulation'],
+                        help='How the probabilities should be calculated (default: integration)')
+    parser.add_argument('-n', type=int, default=10000,
+                        help='Number of points to use for simulation (default 10 000)')
+    parser.add_argument('--printfreq', type=int, default=20,
+                        help='How often to print the current probabilities in numb of function evals (default: 20).\n' +
+                            '0 means never print')
+    parser.add_argument('--targets', type=float, nargs=2, default=(0.3, 0.1),
+                        help='Tail probabilities to target. (default: 0.3 0.1)')
+    parser.add_argument('--locscaleR', type=float, nargs=2, default=(0, 2),
+                        help='location-scale pair for the rate random var R (default 0.0 2.0)')
+    parser.add_argument('--locscaleV0', type=float, nargs=2, default=(14, 11),
+                        help='location-scale pair for the init value random var V_0 (default 14.0 11.0)')
+    parser.add_argument('--initparams', type=float, nargs=4, default=(1.3, 1.05, 2, 6),
+                        help='Initial beta distribution parameters (default: 1.3 1.05 2.0 6.0')
+    parser.add_argument('--lowerbounds', type=float, nargs=4, default=(1e-8, 1e-8, 1e-8, 1e-8),
+                        help='Lower bounds for beta parameters during optimization (default: 1e-8 1e-8 1e-8 1e-8)')
+    parser.add_argument('--upperbounds', type=lambda s: None if s is None else float(s), 
+                        nargs=4, default=(None, None, None, None),
+                        help='Lower bounds for beta parameters during optimization (default: None None None None)')
+    parser.add_argument('--maxiter', type=int, default=4*200, #Numb_of_params * 200
+                        help='Max number of solver iterations (default: 800)')
+    parser.add_argument('--tol', type=float, default=1e-8,
+                        help='Solver tolerance for convergence (default: 1e-8)')
+    parser.add_argument('--solver', choices=['Nelder-Mead'], default='Nelder-Mead')
+    
+    # Parse for ease of use in further code
+    args = parser.parse_args()
+    # Method
+    if args.method == 'integration':
+        get_tails = get_tails_integ
+    elif args.method == 'simulation':
+        get_tails = lambda R, V_0: get_tails_empir(R, V_0, size=args.n)
+    # Location and scales
+    args.locR, args.scaleR = args.locscaleR
+    args.locV0, args.scaleV0 = args.locscaleV0
+    # Bounds
+    args.bounds = zip(args.lowerbounds, args.upperbounds)
+    # Initparams to array
+    args.initparams = np.array(args.initparams)
 
-        Parameters:
-            params (tuple): Parameters of the distribution to optimize (alpha, beta, eta, gamma)
-            early_target (float): Target for ratio of patients who recover too quickly
-            late_target (float): Target for ratio of patients who recover too slowly
-            counter (Counter): Keeps track of number of times this function is evaluated
-            print_frequency (int): How many function evals between prints. 0 means never print.
-    '''
-    if empirical is not None:
-        early_prob, late_prob = get_tails_empir(*params, size=empirical)
-    else:
-        early_prob, late_prob = get_tails_integ(*params)
 
-    # Print
-    if print_frequency > 0 and counter.val % print_frequency == 0: 
-        print(f"Recovery tails: {early_prob, late_prob}")
-    counter.inc()
+    # Function to optimize 
+    def optim_fun(params, counter):
+        # Random variables for visual score defined in simulating_and_pickling as -R*t+V_0
+        # Have support [loc, loc+scale]
+        R = stats.beta(*params[:2], loc=args.locR, scale=args.scaleR)
+        V_0 = stats.beta(*params[2:], loc=args.locV0, scale=args.scaleV0)
 
-    return (early_prob-early_target)**2 + (late_prob-late_target)**2
+        tails = get_tails(R, V_0)
 
+        # Print
+        if args.printfreq > 0 and counter.val % args.printfreq == 0: 
+            print(f"Recovery tails: {tails}")
+        counter.inc()
 
+        return error_fun(tails, args.targets)
 
+    #Minimize
+    counter = Counter()
+    res = optimize.minimize(optim_fun, args.initparams, bounds=args.bounds,
+        args=(counter,), 
+        method=args.solver, options={'maxiter':args.maxiter},
+        tol=args.tol)
 
-initial_params = (5,4,3,2)
-bounds = [(1,None)]*4 #so the beta's pdf are unimodal
-targets = (0.3, 0.1)
-print_frequency = 100
-method = 'Nelder-Mead' #Same solver as default in R
-res = optimize.minimize(optim_fun, initial_params, bounds=bounds,
-                        args=(*targets, Counter(), print_frequency), 
-                        method=method, options={'maxiter':8000, 'maxfev':8000})
-print(res)
-if empirical is not None:
-    print(f"Recovery tails: {get_tails_empir(*res.x, size=empirical)}")
-else:
-    print(f"Recovery tails: {get_tails_empir(*res.x)}")
+    #Result
+    print(res)
+    R = stats.beta(*res.x[:2], loc=args.locR, scale=args.scaleR)
+    V_0 = stats.beta(*res.x[2:], loc=args.locV0, scale=args.scaleV0)
+    tails = get_tails(R, V_0)
+    print(f"Recovery tails: {tails}")
 
 
-# Show plots of found distributions
+    # Show plots of found distributions
 
-fig, axes = plt.subplots(1,2)
-fig.suptitle('Found distribution for visual score recovery, which goes like -R*t + V_0')
-#R
-x = np.linspace(0,2, 5000)
-y = stats.beta(res.x[0], res.x[1], loc=0, scale=2).pdf(x)
-axes[0].plot(x, y)
-axes[0].set_title("PDF of R")
-#V_0
-x = np.linspace(14,25, 5000)
-y = stats.beta(res.x[2], res.x[3], loc=14, scale=11).pdf(x)
-axes[1].plot(x, y)
-axes[1].set_title("PDF of V_0")
+    fig, axes = plt.subplots(1,2)
+    fig.suptitle('Found distribution for visual score recovery, which is used like -R*t + V_0\n' +
+                f'obtaining recovery time tails of {tails[0]:g}, {tails[1]:g}')
+    #R
+    x = np.linspace(args.locR, args.locR+args.scaleR, 5000)
+    y = R.pdf(x)
+    axes[0].plot(x, y)
+    axes[0].set_title(f"PDF of R \nParams: {res.x[0]:g}, {res.x[1]:g}")
+    #V_0
+    x = np.linspace(args.locV0, args.locV0+args.scaleV0, 5000)
+    y = V_0.pdf(x)
+    axes[1].plot(x, y)
+    axes[1].set_title(f"PDF of V_0 \nParams: {res.x[2]:g}, {res.x[3]:g}")
 
-plt.show()
+    plt.tight_layout()
+    plt.show()
