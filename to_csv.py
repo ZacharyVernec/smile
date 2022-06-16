d78b058e4b7a38504b903a8e98a639fc5a74cd30
Zachary Vernec <zachary.vernec@gmail.com>
2022-06-16 17:09:52 -0400

    Create csvs of only days

--- to_csv.py
+++ to_csv.py
@@ -1,11 +1,10 @@
 # Standard library imports
 import os
-from datetime import datetime
-import re
 
 # Third party imports
 import numpy as np
 import dill
+import pandas as pd
 
 # Local application imports
 #none
@@ -17,8 +16,8 @@ import dill
 seed = 3 # chosen by fair dice roll. guaranteed to be random. https://xkcd.com/221/
 np.random.seed(seed)
 np.set_printoptions(edgeitems=30, linewidth=100000)
-pickle_pops_dir = r'C:\Users\zachv\Desktop\simulating_randomness_test_11'
-pickle_csv_dir = r'C:\Users\zachv\Desktop\simulating_randomness_test_11'
+pickle_pops_dir = r'C:\Users\zachv\Desktop\smile_desk\simulating_randomness_test_11'
+pickle_csv_dir = r'C:\Users\zachv\Desktop\smile_desk\simulating_randomness_test_11_csv'
 
 
 
@@ -59,11 +58,12 @@ def options_to_string(slope, error):
     return f"{slope}_{error}".replace(".","")
 
 npops_per_sim = 2
-nsims = npops // npops_per_sim #TODO error if less pops than pops per sim
+nsims = npops // npops_per_sim
 npops_remainder = npops % npops_per_sim
 if npops_remainder > 0: nsims += 1
 
 for n in range(nsims):
+    #Full
     poplists = load_from_file(pickle_pops_dir+"\worddoc_poplists_"+str(n)+".pik")
     for i,j in np.ndindex(poplists.shape):
         foldername = f"realistic_"+options_to_string(slope_options[i], error_options[j])
@@ -73,7 +73,7 @@ for n in range(nsims):
             dump_to_csv_file(pop.to_dataframe(), foldername+f"_population_{n*npops_per_sim+l}", filesuffix='.csv', 
                              dirname=os.path.join(pickle_csv_dir, foldername), create_newdir=True, avoid_overwrite=True)
 
-
+    #Filtered
     poplists = load_from_file(pickle_pops_dir+"\worddoc_filtered_poplists_"+str(n)+".pik")
     for i,j in np.ndindex(poplists.shape):
         foldername = f"realistic_"+options_to_string(slope_options[i], error_options[j])
@@ -83,13 +83,23 @@ for n in range(nsims):
             dump_to_csv_file(pop.to_dataframe(), foldername+f"_population_{n*npops_per_sim+l}_filtered", filesuffix='.csv', 
                              dirname=os.path.join(pickle_csv_dir, foldername), create_newdir=True, avoid_overwrite=True)
 
-
+    #Sampled
     poplists = load_from_file(pickle_pops_dir+"\worddoc_sampled_poplists_"+str(n)+".pik")
     for i,j, k in np.ndindex(poplists.shape):
         foldername = f"realistic_"+options_to_string(slope_options[i], error_options[j])
         poplist = poplists[i,j,k]
         for l in range(len(poplist)):
             pop = poplist[l]
-            dump_to_csv_file(pop.to_dataframe(), foldername+f"_population_{n*npops_per_sim+l}_sampled_{method_options[k]}", filesuffix='.csv', 
+            df = pop.to_dataframe()
+            #Basic
+            dump_to_csv_file(df, foldername+f"_population_{n*npops_per_sim+l}_sampled_{method_options[k]}", filesuffix='.csv', 
                              dirname=os.path.join(pickle_csv_dir, foldername), create_newdir=True, avoid_overwrite=True)
+            #Days only
+            dfscoreless = df[['person', 'day']] #remove scores
+            dflists = dfscoreless.groupby('person').agg(list) #aggregate each person's days into lists
+            dfdays = pd.DataFrame(dflists['day'].to_list()).set_index(dflists.index) #split lists back into columns
+            dump_to_csv_file(dfdays, foldername+f"_population_{n*npops_per_sim+l}_sampled_{method_options[k]}_daysonly", filesuffix='.csv',
+                             dirname=os.path.join(pickle_csv_dir, foldername), create_newdir=True, avoid_overwrite=True)
+
+            
 # use python -u to_csv.py 2>&1 | tee to_csv_out.txt
